144d6b315901716ec9984fc487a5d64c1c4ae273
kumkum bhana <kumkumbhana@gmail.com>
2023-01-24 11:38:46 +0530

    Updates

---
+++ precache-manifest.df88b4778a88eb87473ce9a88ad3f083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8150dabf9ff18f96c2c4",
+    "url": "/Todos-Todo-List/static/css/main.19cf9fe2.chunk.css"
+  },
+  {
+    "revision": "8150dabf9ff18f96c2c4",
+    "url": "/Todos-Todo-List/static/js/main.8150dabf.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/Todos-Todo-List/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "c23ba486b22546340467",
+    "url": "/Todos-Todo-List/static/js/2.c23ba486.chunk.js"
+  },
+  {
+    "revision": "74ada0f39cb3cfe29749",
+    "url": "/Todos-Todo-List/static/js/runtime~main.74ada0f3.js"
+  },
+  {
+    "revision": "d4a132e2fad552689d4f288f8214d924",
+    "url": "/Todos-Todo-List/index.html"
+  }
+];
